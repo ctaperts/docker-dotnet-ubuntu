adeed16a3004336519a13e1ffe93a2443b81ab15
Colby Taperts <colbytaperts@gmail.com>
2017-12-17 20:59:13 -0600

    create variables for the run

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker build -f ./Dockerfile-sdk-only -t colbyt/dotnet-ubuntu .
+DOCKER_REPO=colbyt
+DOCKER_NAME=dotnet-ubuntu
+DOCKER_FILE=./Dockerfile-sdk-only
+
+docker build -f $DOCKER_FILE -t $DOCKER_REPO/$DOCKER_NAME .
 docker push colbyt/dotnet-ubuntu
